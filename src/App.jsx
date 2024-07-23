933d092aec9aad9286ca2d60cbc85a798c1cf627
Sunil Kumar Malani <sunilmalani456@gmail.com>
2024-07-24 00:19:46 +0530

    added hero section page

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,31 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import LogIn from "./components/auth/log-in";
+import Hero from "./pages/home/hero";
 
 function App() {
-  return <main>App page</main>;
+  return (
+    <div className="absolute top-0 z-[-2] h-screen w-screen bg-[#000000] bg-[radial-gradient(#ffffff33_1px,#00091d_1px)] bg-[size:20px_20px]">
+      <Router>
+        <Routes>
+          <Route
+            path="/"
+            element={<Hero />}
+            // element={authUser ? <Home /> : <Navigate to={"/login"} />}
+          />
+          <Route
+            path="/login"
+            element={<LogIn />}
+            // element={authUser ? <Navigate to="/" /> : <Login />}
+          />
+          <Route
+            path="/signup"
+            // element={authUser ? <Navigate to="/" /> : <SignUp />}
+          />
+        </Routes>
+      </Router>
+      {/* <Toaster /> */}
+    </div>
+  );
 }
 
 export default App;
