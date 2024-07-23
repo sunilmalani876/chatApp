18637a3f59b2b170f7ce56473960921cd9f14eb6
sunilmalani876 <sunilkumarmalani5@gmail.com>
2024-07-23 18:58:31 +0530

    setup complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 function App() {
-  return <div>Home page</div>;
+  return <main>App page</main>;
 }
 
 export default App;
